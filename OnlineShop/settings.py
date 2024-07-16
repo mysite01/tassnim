826852ad2aa1cfdfeb2f22608538d62cf4c2ff56
Tassnim <s90301@bht-berlin.de>
2024-07-16 05:47:56 +0200

    Zweiter Commit

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-0sgusr#%8k)_mmo+e=56q%0y0mxo^z)&pcl-&#v@ldh139%1&c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tassnim.onrender.com']
 
 # Application definition
 
@@ -121,7 +121,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'home'
+#LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 MEDIA_ROOT = BASE_DIR / 'media/'
